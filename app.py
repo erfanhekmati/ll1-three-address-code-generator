2d5b6758c4f394bc5068743fa598385f0ee3d0f9
erfanhekmati <59681845+erfanhekmati@users.noreply.github.com>
2021-02-06 14:07:57 +0330

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from read import readGrammer, readInput
 
 myLL1 = LL1(*readGrammer('grammer1.txt'))
 codes = myLL1.getThreeAddressCode(readInput('input.txt'))
-#print("wdwad", codes)
+
 print("\n\n\n")
 for code in codes:
     print(code)
